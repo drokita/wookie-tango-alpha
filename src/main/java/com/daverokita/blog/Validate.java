51c8cdc36379261598e129f583e56e08af51474c
drokita <drokita@hotmail.com>
2016-07-28 16:17:25 -0500

    Blog v0.1.1 - bcrypt

--- src/main/java/com/daverokita/blog/Validate.java
+++ src/main/java/com/daverokita/blog/Validate.java
@@ -1,10 +1,14 @@
 package com.daverokita.blog;
 
 import java.sql.*;
+import org.mindrot.jbcrypt.BCrypt;
+import java.security.NoSuchAlgorithmException;
 
 public class Validate {
     public static boolean checkUser(String email, String password) {
-        boolean st = false;
+        boolean validated = false;
+        String hash;
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
@@ -12,15 +16,15 @@ public class Validate {
             Connection con = DriverManager.getConnection
                     ("jdbc:mysql://192.168.187.159:3306/blog", "blogadmin", "tech1200");
             PreparedStatement ps = con.prepareStatement
-                    ("select * from USERS where email=? and password=?");
+                    ("select hash from USERS where email=?");
             ps.setString(1, email);
-            ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
-            st = rs.next();
+            rs.next();
+            hash = rs.getString("hash");
+            validated = BCrypt.checkpw(password, hash);
         } catch(Exception e) {
             e.printStackTrace();
         }
-
-        return st;
+        return validated;
     }
 }
